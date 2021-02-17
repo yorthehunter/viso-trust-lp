aa3c32213ad95d8ce191e78a8f1c7f5fdd0b0fb5
yorthehunter <boru.design@gmail.com>
2021-02-17 10:42:51 -0500

    Applies Material UI CSSBaseline (browser reset); updates meta tag; creates a button to test component structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Button from '@material-ui/core/Button';
 
 const App = () => (
   <div className="App">
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
-</div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <Button variant="contained" color="primary">
+        Material UI Button
+      </Button>
+    </header>
+  </div>
 )
 
 export default App;
