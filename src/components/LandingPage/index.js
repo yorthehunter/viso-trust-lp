33cf9c3d3ece05941cd64c76665d5a86d56b6e6c
yorthehunter <boru.design@gmail.com>
2021-02-26 23:01:39 -0500

    Adds href to main CTA

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -11,7 +11,7 @@ const LandingPage = () => {
           <header className={style.header}>
             <h1>Third party cyber risk at the speed of business.</h1>
             <h2>65% of public security breaches occur due to the failure of third parties to make good on their security commitments.</h2>
-            <CTA size="large" variant="primary">Schedule a Demo</CTA>
+            <CTA size="large" variant="primary" href="https://calendly.com/visotrust-team/product-demo">Schedule a Demo</CTA>
           </header>
           <img className={style.heroImage} src={heroImage} alt="Demo screenshots of the VISO Trust app in action" />
         </div>
