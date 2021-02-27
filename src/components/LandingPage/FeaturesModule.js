846ea6d6e21628297672f1791cd19204e6fb88f0
yorthehunter <boru.design@gmail.com>
2021-02-27 00:13:26 -0500

    Next module set up

---
+++ src/components/LandingPage/FeaturesModule.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './featuresModule.module.scss'
+
+const FeaturesModule = () => (
+  <section className={style.featuresModule}>
+    <div className={style.content}>
+      <header className={style.header}>
+        <h2>VISO Trust Features</h2>
+      </header>
+    </div>
+  </section>
+)
+
+export default FeaturesModule
--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroModule from './HeroModule'
 import TestimonialsModule from './TestimonialsModule'
+import FeaturesModule from './FeaturesModule'
 
 const LandingPage = () => {
   return (
     <main>
       <HeroModule />
       <TestimonialsModule />
+      <FeaturesModule />
     </main>
   )
 }
